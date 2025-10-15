839b01ae188549eadc6309fddb424de9561b6de3
coung21 <coungdev@gmail.com>
2025-10-15 18:51:04 +0700

    rm color jitter

--- train.py
+++ train.py
@@ -364,7 +364,6 @@ def train_model():
         transforms.RandomHorizontalFlip(),
         transforms.RandomVerticalFlip(),
         transforms.RandomRotation(15),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], 
                            std=[0.229, 0.224, 0.225])
